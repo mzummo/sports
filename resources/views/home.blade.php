63bd0031a83a90af4ff886871906f40a986a8e1e
Mathew Zummo <mzummo@gmail.com>
2018-12-16 17:53:26 -0500

    looks better
    Signed-off-by: Mathew Zummo <mzummo@gmail.com>

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div id="application" class="container">
+        <div id="application" class="container" style="opacity: 0.8;">
                 <sports-app></sports-app>
         </div>
     </div>
